0ff05af87c8218e45b2474534b8ac9767c23f96b
Darlan Alves <me@darlanalv.es>
2023-04-28 20:45:25 +0000

    fix: do not write back to files in constructor

--- src/file-map.ts
+++ src/file-map.ts
@@ -11,19 +11,26 @@ function writeFile(storagePath: string, file: string, content: any) {
 
 function listFiles(storagePath: string) {
   const files = readdirSync(storagePath, { withFileTypes: true });
-  return files.filter(f => f.isFile()).map(f => f.name);
+  return files.filter((f) => f.isFile()).map((f) => f.name);
 }
 
 export class FileMap extends Map<string, any> {
+  protected enableWrite = false;
+
   constructor(protected storagePath: string) {
     const files = listFiles(storagePath);
-    const entries: any = [files.map(file => [file, readFile(storagePath, file)])];
+    const entries: any = [files.map((file) => [file, readFile(storagePath, file)])];
     super(entries);
+    this.enableWrite = true;
   }
 
   set(key: string, value: any) {
     const v = super.set(key, value);
-    writeFile(this.storagePath, key, value);
+
+    if (this.enableWrite) {
+      writeFile(this.storagePath, key, value);
+    }
+
     return v;
   }
 }
